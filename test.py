c2ad26ff9c723a9fd0bae13f048a2ff032b42540
jekolsh <ek.olshannikova@gmail.com>
2022-10-06 13:09:50 +0200

    Raw version of the game

---
+++ test.py
@@ -0,0 +1,15 @@
+frågor = {
+    "Varför kallas språket så? :" : "B",
+    "Var är Python i TIOBE 2021-ränkingen? : " : "C",
+    "Pythons kärnfilosofi sammanfattas i dokumentet 'The Zen of Python' PEP 203. Vilket av dessa påståenden gäller inte Pythons filosofi?: \n " : "C"
+    "Vilket av dessa program är INTE skrivet i Python? : " : "C",
+    "Vad betyder 'unpythonic' i Python community? : " : "B"
+
+}
+
+alternativ = [
+["A. Namnet kommer från en reptil", "B. Namnet på språket kommer från BBC komediserie Monty Python's Flying Circus", "C. Efternamnet på skaparen är Python","D. Svar 2 och 3"],
+["A. Tredje plats", "B. Andra plats", "C. Första plats","D. Fjarde plats"],
+["A. Läsbarheten räknas", "B. Vackert är bättre än fult", "C. Det bästa är fiende till det goda","D. Inga av de"],
+["A. Reddit", "B. Pinterest", "C. Adobe Photoshop","D. Adobe illstrator"],
+["A. Kod med många fel", "B. Kod som är svår att förstå eller läsa", "C. Kod som skriven på ett annat programmeringsspråk","D. Svar 1 + 2"]]
