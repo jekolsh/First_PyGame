df2eeddf7568184932a8fd5c7ca4ed2ef8f844b7
jekolsh <ek.olshannikova@gmail.com>
2022-10-06 14:55:58 +0200

    Fixed errors with enemies

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -114,12 +115,14 @@ class enemy(object):
         self.end = end
         self.path = [self.x, self.end]
         self.animCount = 0
-        self.vel = 3
+        self.vel = random.randint(1, 5)
         self.hitbox = (self.x + 17, self.y +2, 31, 57)
-        self.health = 10
+        self.health = random.randint(3, 10)
         self.visible = True
 
     def draw(self, win):
+        if  not self.visible:
+            return 
         self.move()
         if self.animCount + 1 >= 33:
             self.animCount = 0
@@ -155,16 +158,11 @@ class enemy(object):
     def hit(self):
         if self.health >= 0:
             self.health  -=1
-            #self.end = end
-            #winner_text = "Goblin Wins!"
-        if  self.health <= 0:
-            #self.end = end
-            #winner_text = "Goblin Wins!"
-            goblin.draw(win)
-            
         else:
+            self.health <= 0
             self.visible = False
-            #winner_text = "Goblin Wins!"
+            goblin.draw(win)
+
         print("hit")
 
 
@@ -182,21 +180,34 @@ def drawWindow():
     pygame.display.update()
 
 #main loop
-font = pygame.font.SysFont('comicsans', 30, True)
+font = pygame.font.SysFont('comicsans', 15, True)
 man = player(300, 410, 60, 71)
 goblin = enemy(100, 410, 64, 64, 450)
-goblin2 = enemy(100, 410, 64, 64, 450)
+goblin2 = enemy(10, 410, 64, 64, 450)
 shootLoop = 0
 bullets = []
 run = True
 while run:
     #drawWindow()
     clock.tick(30)
+    if not goblin.visible and  not goblin2.visible:
+        print("Win-win!")
+                
+    if score < -1:
+        print("You are looser :(")
+        
 
-    if man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1]:
-        if man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
-            man.hit()
-            score -= 5
+
+    if goblin.visible and man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin.hitbox[1] and man.hitbox[0] + man.hitbox[2] > goblin.hitbox[0] and man.hitbox[0] < goblin.hitbox[0] + goblin.hitbox[2]:
+        man.hit()
+        score -= 5
+        goblin.vel = -goblin.vel
+    if goblin2.visible and  man.hitbox[1] < goblin.hitbox[1] + goblin.hitbox[3] and man.hitbox[1] + man.hitbox[3] > goblin2.hitbox[1] and man.hitbox[0] + man.hitbox[2] > goblin2.hitbox[0] and man.hitbox[0] < goblin2.hitbox[0] + goblin2.hitbox[2]:
+        man.hit()
+        score -= 5
+        goblin2.vel = -goblin2.vel
+
+
      
     if shootLoop > 0:
         shootLoop +=1
@@ -206,12 +217,17 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    
     for bullet in bullets:
-        if bullet.y - bullet.radius < goblin.hitbox[1] + goblin.hitbox[3] and bullet.y + bullet.radius > goblin.hitbox[1]:
-            if bullet.x + bullet.radius > goblin.hitbox[0] and bullet.x - bullet.radius < goblin.hitbox[0] + goblin.hitbox[2]:
+        
+        if goblin.visible and bullet.y - bullet.radius < goblin.hitbox[1] + goblin.hitbox[3] and bullet.y + bullet.radius > goblin.hitbox[1] and bullet.x + bullet.radius > goblin.hitbox[0] and bullet.x - bullet.radius < goblin.hitbox[0] + goblin.hitbox[2]:
                 goblin.hit()
                 #goblin2.hit()
+                score += 1
+                bullets.pop(bullets.index(bullet))
+        elif goblin2.visible and bullet.y - bullet.radius < goblin2.hitbox[1] + goblin2.hitbox[3] and bullet.y + bullet.radius > goblin2.hitbox[1] and bullet.x + bullet.radius > goblin2.hitbox[0] and bullet.x - bullet.radius < goblin2.hitbox[0] + goblin2.hitbox[2]:
+                # goblin.hit()
+                goblin2.hit()
                 score += 1
                 bullets.pop(bullets.index(bullet))
 
@@ -268,5 +284,6 @@ while run:
             man.jumpCount = 10
             
     drawWindow()
+
 
 pygame.quit()
